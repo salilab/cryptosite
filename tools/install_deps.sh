fba860d437d278d2371746bca6a61984c066cebf
Ben Webb <ben@salilab.org>
2016-10-29 15:30:44 -0700

    Update conda in root environment.
    Trying to update it in our custom environment
    doesn't work, because technically it's not
    installed there (only symlinked).

--- tools/install_deps.sh
+++ tools/install_deps.sh
@@ -41,10 +41,10 @@ fi
 
 # Make sure that our conda environment is up to date
 export PATH=${conda_dir}/bin:$PATH
+conda update --yes -q conda
 source activate python${python_version}
 
 export KEY_MODELLER=`cat ${modeller_license_file}`
-conda update --yes -q conda
 conda install --yes -c salilab pip biopython scikit-learn scipy modeller nose
 pip install coverage
 
