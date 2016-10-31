d97b33b2f450315b00cd19886c6b9a865725b9ec
Ben Webb <ben@salilab.org>
2016-10-31 13:15:15 -0700

    Add basic test of PREDICTER.

---
+++ test/test_PREDICTER.py
@@ -0,0 +1,23 @@
+import unittest
+import utils
+import os
+import sys
+import re
+import subprocess
+
+TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(TOPDIR)
+import PREDICTER
+
+class Tests(unittest.TestCase):
+
+    def test_get_matrix(self):
+        """Test get_matrix() function"""
+        fname = os.path.join(TOPDIR, 'test', 'input', 'test.features')
+        m, header, indices = PREDICTER.get_matrix(fname, model='final')
+        self.assertEqual(len(m), 2)
+        self.assertEqual(header, ['SQC', 'PTM', 'CNC_mean_'])
+        self.assertEqual(len(indices), 2)
+
+if __name__ == '__main__':
+    unittest.main()
