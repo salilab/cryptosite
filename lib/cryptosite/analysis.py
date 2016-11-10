2eeb26c693f72a2f269a827d06b0ca08bb581703
Ben Webb <ben@salilab.org>
2016-11-09 18:08:41 -0800

    Implement and test analysis get_qi functionality.

--- lib/cryptosite/analysis.py
+++ lib/cryptosite/analysis.py
@@ -31,9 +31,98 @@ def get_energy(landscape):
         with open(os.path.join(dirname, 'energy.dat'), 'w') as energy:
             energy.write(''.join(energy_lines[-nmodels:]))
 
+class _AveragedAtom(object):
+    """A fake atom whose coordinates are the average of other atoms."""
+    def __init__(self, atoms):
+        self.x = self.y = self.z = 0.
+        for a in atoms:
+            self.x += a.x
+            self.y += a.y
+            self.z += a.z
+        self.x /= len(atoms)
+        self.y /= len(atoms)
+        self.z /= len(atoms)
+
+def get_coordinates_sc(m, fname):
+    """Get the coordinates of the sidechain of each residue in the file"""
+    m.read(file=fname)
+    def get_coord(res):
+        if res.name == 'GLY':
+            if 'CA' in res.atoms:
+                return res.atoms['CA']
+        else:
+            avg_atoms = [a for a in res.atoms
+                         if a.name not in ('CA', 'O', 'N', 'C', 'OT')
+                         and not a.name.startswith('H')]
+            if avg_atoms:
+                return _AveragedAtom(avg_atoms)
+    return [get_coord(r) for r in m.residues]
+
+def get_distance(ci, cj):
+    if ci is not None and cj is not None:
+        dx = ci.x - cj.x
+        dy = ci.y - cj.y
+        dz = ci.z - cj.z
+        return dx*dx + dy*dy + dz*dz
+
+def get_distances(coord, rcut):
+    rcut2 = rcut * rcut
+    dist = {}
+    for i in range(len(coord) - 1):
+        for j in range(i + 1, len(coord)):
+            d = get_distance(coord[i], coord[j])
+            if d is not None and d < rcut2:
+                dist[(i,j)] = dist[(j,i)] = math.sqrt(d)
+    return dist
+
+def get_qi(m, len_coord, dist, template, fh):
+    coord = get_coordinates_sc(m, template)
+    if len(coord) != len_coord:
+        raise ValueError("different numbers of residues")
+    qi_cut = [0.] * len(coord)
+    nqi_cut = [0] * len(coord)
+    for i in range(len(coord)):
+        for j in range(len(coord)):
+            if abs(i-j) < 2 or (i,j) not in dist:
+                continue
+            d = get_distance(coord[i], coord[j])
+            if d is not None:
+                delta = (dist[(i,j)] - math.sqrt(d)) / (abs(j-i) ** 0.15)
+                qi_cut[i] += math.exp(-delta * delta * 0.5)
+                nqi_cut[i] += 1
+    for i in range(len(coord)):
+        if nqi_cut[i] > 0:
+            qi_cut[i] /= nqi_cut[i]
+        else:
+            qi_cut[i] = 1.1
+        fh.write("%.4f " % qi_cut[i])
+
+def get_qioft(landscape, rcut=11.):
+    """Calculate Qi for all models in a landscape."""
+    import modeller
+    modeller.log.none()
+    e = modeller.environ()
+    e.io.hetatm = False
+    m = modeller.model(e)
+
+    for dirname in _get_subdirectories(landscape):
+        with open(os.path.join(dirname, 'list')) as fh:
+            temp1 = 'pm_' + fh.readline().strip()
+            pm = os.path.join(dirname, temp1)
+        models = sorted(glob.glob(os.path.join(dirname, 'pm.pdb.B[1-8]*.pdb')))
+    
+        coord = get_coordinates_sc(m, pm)
+        dist = get_distances(coord, rcut)
+        with open(os.path.join(dirname, 'qioft_%s_%dsc.dat'
+                                         % (temp1, int(rcut))), 'w') as fh:
+            for model in models:
+                get_qi(m, len(coord), dist, model, fh)
+                fh.write('\n')
+
 def main():
     for landscape in sys.argv[1:]:
         get_energy(landscape)
+        get_qioft(landscape)
 
 if __name__ == '__main__':
     main()
--- test/test_analysis.py
+++ test/test_analysis.py
@@ -36,5 +36,20 @@ class Tests(unittest.TestCase):
                      '   1000      13580.25098   0.0279   0.0794       '
                      '3332.75293        493.31027\n'])
 
+    def test_get_qioft(self):
+        """Test get_qioft() function"""
+        inputs = os.path.join(TOPDIR, 'test', 'input')
+        with utils.temporary_directory() as tmpdir:
+            subdir = os.path.join(tmpdir, 'XXX.pdb_14')
+            os.mkdir(subdir)
+            for f in ('list', 'pm.pdb.B10010001.pdb', 'pm_XXX.pdb'):
+                shutil.copy(os.path.join(inputs, f), subdir)
+
+            cryptosite.analysis.get_qioft(tmpdir)
+            with open(os.path.join(subdir, 'qioft_pm_XXX.pdb_11sc.dat')) as fh:
+                e = fh.readlines()
+            self.assertEqual(e, ['0.2205 0.3422 0.5290 0.3791 0.2468 '
+                         '0.4816 0.3841 0.5149 0.4896 0.0971 0.3187 0.0189 \n'])
+
 if __name__ == '__main__':
     unittest.main()
