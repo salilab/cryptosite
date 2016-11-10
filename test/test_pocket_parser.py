850f6af90b99756e572b06803e40f55efd6734e6
Ben Webb <ben@salilab.org>
2016-11-10 15:16:20 -0800

    Test simple complete run of pocket_parser.

--- test/test_pocket_parser.py
+++ test/test_pocket_parser.py
@@ -4,9 +4,10 @@ import os
 import sys
 import re
 import subprocess
+import shutil
 
 TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(os.path.join(TOPDIR, 'lib'))
+utils.set_search_paths(TOPDIR)
 import cryptosite.pocket_parser
 
 class Tests(unittest.TestCase):
@@ -18,6 +19,21 @@ class Tests(unittest.TestCase):
                                                None)
         self.assertEqual(len(res), 8)
         self.assertEqual(res[('ILE', 9, 'A')], 0.0)
+
+    def test_main(self):
+        """Test simple complete run of pocket_parser"""
+        with utils.temporary_working_directory() as tmpdir:
+            shutil.copy(os.path.join(TOPDIR, 'test', 'input',
+                                     'pm.pdb.B10010001.pdb'), '.')
+            with open('SnapList.txt', 'w') as fh:
+                fh.write("pm.pdb.B10010001.pdb -100.0\n")
+                fh.write("high-energy.pdb -10.0\n")
+            subprocess.check_call(['cryptosite', 'pocket_parser'])
+            with open('pockets.out') as fh:
+                lines = fh.readlines()
+        self.assertEqual(len(lines), 13)
+        self.assertEqual(lines[1], 'ALA\t1\tA\t0.0\n')
+        self.assertEqual(lines[2], 'MET\t2\tA\t0.092\n')
 
 if __name__ == '__main__':
     unittest.main()
