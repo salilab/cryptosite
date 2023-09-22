1cd19e72345cc3c7263925b97fc24045fb31f080
Ben Webb <ben@salilab.org>
2023-09-21 22:29:17 -0700

    Use IOError, not OSError, for Python 2

--- lib/cryptosite/bmi_feature_parser.py
+++ lib/cryptosite/bmi_feature_parser.py
@@ -15,7 +15,7 @@ def get_cnc(apo):
     try:
         with open('%s_out/%s_out.pdb' % (apo, apo)) as data:
             D = data.readlines()
-    except OSError:
+    except IOError:
         # If fpocket finds no pockets, it does not create the output directory
         return {}, ('1', '1')
 
